20f797bb1b0bad94fbc4fac33928b3d79e9a9fc4
Prath-Maharshi <digvijaysingh.parihar@iitgn.ac.in>
2025-04-15 06:36:13 +0530

    For initializing setup
    Include vcpkg inside NodeEditor when running this.

---
+++ NodeEditor/build/CMakeFiles/NodeEditor.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NodeEditor.dir/NodeEditor_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/NodeEditor.dir/NodeEditor_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/NodeEditor.dir/main.cpp.obj"
+  "CMakeFiles/NodeEditor.dir/main.cpp.obj.d"
+  "CMakeFiles/NodeEditor.dir/src/MainWindow.cpp.obj"
+  "CMakeFiles/NodeEditor.dir/src/MainWindow.cpp.obj.d"
+  "CMakeFiles/NodeEditor.dir/src/NodeCanvas.cpp.obj"
+  "CMakeFiles/NodeEditor.dir/src/NodeCanvas.cpp.obj.d"
+  "CMakeFiles/NodeEditor_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/NodeEditor_autogen.dir/ParseCache.txt"
+  "NodeEditor_autogen"
+  "NodeEditor.exe"
+  "NodeEditor.exe.manifest"
+  "NodeEditor.pdb"
+  "NodeEditor_autogen/mocs_compilation.cpp"
+  "NodeEditor_autogen/timestamp"
+  "libNodeEditor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NodeEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NodeEditor/build/CMakeFiles/NodeEditor_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for NodeEditor_autogen.
+# This may be replaced when dependencies are built.
