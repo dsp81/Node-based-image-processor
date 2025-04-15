fb50e5e3314b5f6544b12c3b5382d5ded9bfd970
dsp81 <121604590+dsp81@users.noreply.github.com>
2025-04-15 06:27:15 +0530

    This is the intital C++ file for checking.

---
+++ NodeEditor/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include "src/MainWindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.setWindowTitle("Node Editor");
+    window.resize(800, 600);
+    window.show();
+
+    return app.exec();
+}
